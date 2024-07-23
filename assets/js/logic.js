99b15bd4fe7b6e24728916cbe919f802983a39e7
Bilenie Tilahun <130017473+Bilenie@users.noreply.github.com>
2024-07-22 21:00:12 -0500

    Updated logic.js to persist the modes through changing pages.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,21 +1,34 @@
+const modeToggle = document.getElementById('mode-toggle');
+const modeIcon = document.getElementById('mode-icon');
 
-  const modeToggle = document.getElementById('mode-toggle');
-  const modeIcon = document.getElementById('mode-icon');
+// Function to toggle dark mode
+modeToggle.addEventListener('change', function() {
+  if (modeToggle.checked) {
+    enableDarkMode();
+  } else {
+    enableLightMode();
+  }
+});
 
-  modeToggle.addEventListener('change', function() {
-    if (modeToggle.checked) {
-      document.body.classList.add('dark-mode');
-      modeIcon.textContent = '🌞'; // Emoji for crescent moon (dark mode)
-    } else {
-      document.body.classList.remove('dark-mode');
-      modeIcon.textContent = '🌙'; // Emoji for sun (light mode)
-    }
-  });
+// Function to enable dark mode
+function enableDarkMode() {
+  document.body.classList.add('dark-mode');
+  modeIcon.textContent = '🌙'; // Emoji for sun (light mode)
+  localStorage.setItem('isDarkMode', true); // Store dark mode state
+}
 
-  // Initial state based on localStorage or default
-  const isDarkMode = JSON.parse(localStorage.getItem('isDarkMode')) || false;
-  if (isDarkMode) {
-    document.body.classList.add('dark-mode');
-    modeIcon.textContent = '🌙';
-    modeToggle.checked = true;
-  };
+// Function to enable light mode
+function enableLightMode() {
+  document.body.classList.remove('dark-mode');
+  modeIcon.textContent = '🌞'; // Emoji for crescent moon (dark mode)
+  localStorage.setItem('isDarkMode', false); // Store light mode state
+}
+
+// Initial state based on localStorage or default
+const isDarkMode = JSON.parse(localStorage.getItem('isDarkMode')) || false;
+if (isDarkMode) {
+  enableDarkMode();
+  modeToggle.checked = true;
+} else {
+  enableLightMode();
+}
